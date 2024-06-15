2845ee6f78dfafc9620657e11680a2b032706339
John Cleve <johnpcleve@gmail.com>
2024-06-15 18:16:29 -0400

    Update entrypoint.rb

--- entrypoint.rb
+++ entrypoint.rb
@@ -23,7 +23,7 @@ GithubChecksVerifier.configure do |config|
   config.client.api_endpoint = api_endpoint unless /\A[[:space:]]*\z/.match?(api_endpoint)
   config.client.access_token = token
   config.ref = ref
-  config.repo = repo
+  config.repo = "sas-institute-rnd-ci360/ci360-cldeng-e2e-nightly"
   config.verbose = verbose
   config.wait = wait.to_i
   config.workflow_name = workflow_name  
